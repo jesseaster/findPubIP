ab80649043a35de2e6c546c9e224b1732be2459b
J Easter <justineaster@gmail.com>
2018-07-07 20:17:39 -0400

    run continuously

--- findPubIP.sh
+++ findPubIP.sh
@@ -1,1 +1,1 @@
-ssh root@192.168.1.1 ifconfig | awk '/inet/{i++}i==4{print; exit}' > pubip.txt
+sleep 40 && while true; do ssh root@192.168.1.1 ifconfig | awk '/inet/{i++}i==4{print; exit}' > pubip.txt; sleep 40; done
